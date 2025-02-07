deefeb3ab375cb9bc2ae492d4c58fe30fef2c20b
Daopos <ivan.munoz.50309277@gmail.com>
2025-02-07 23:13:32 +0800

    adding animation to the navbar

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
-const navbar = document.getElementsByTagName('nav')[0]; 
+const navbar = document.getElementsByTagName("nav")[0];
+const navItems = document.querySelectorAll(".nav-items > ul li");
 
-window.addEventListener('scroll', () => {
-    if (window.scrollY > 50) {
-        console.log("It's working");
-        navbar.classList.add('scrolled');
-    } else {
-        navbar.classList.remove('scrolled');
-    }
+window.addEventListener("scroll", () => {
+  console.log("Scroll position: ", window.scrollY); // For debugging
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+    navItems.forEach((item) => {
+      item.classList.add("small-font"); // Add the small font class
+    });
+  } else if (window.scrollY < 10) {
+    navbar.classList.remove("scrolled");
+    navItems.forEach((item) => {
+      item.classList.remove("small-font"); // Remove the small font class
+    });
+  }
 });
